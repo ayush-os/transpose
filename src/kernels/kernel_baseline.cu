c3c5c5d3e770939de0d1bbf79e80ce03f3158897
ayush <99374392+ayush-os@users.noreply.github.com>
2025-12-04 19:17:40 -0800

    trying something out

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -15,11 +15,12 @@ __global__ void baseline_matrix_transpose_kernel(float *output,
   int oldSpot = threadIdX + N * threadIdY;
   int newSpot = threadIdY + N * threadIdX;
 
-  if (threadIdX <= 6 && threadIdY <= 6) {
-    printf("===============threadIdX: %d, threadIdY: "
-           "%d===============\noldSpot: %d\nnewSpot: %d\n===============\n",
-           threadIdX, threadIdY, oldSpot, newSpot);
-  }
+  // if (threadIdX <= 6 && threadIdY <= 6) {
+  //   printf("===============threadIdX: %d, threadIdY: "
+  //          "%d===============\noldSpot: %d\nnewSpot: %d\n===============\n",
+  //          threadIdX, threadIdY, oldSpot, newSpot);
+  // }
 
-  output[newSpot] = input[oldSpot];
+  // output[newSpot] = input[oldSpot];
+  output[oldSpot] = input[newSpot];
 }
